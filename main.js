60a8d7cea883b1251646cea6e4e780e3e3c39823
alice <aalice851215@gmail.com>
2023-09-06 01:14:49 +0800

    updata

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-console.log("Hello world!");
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    scrollbar: {
+        el: ".swiper-scrollbar"
+    }, 
+    breakpoints: {
+        1280: {
+            slidesPerView: 4
+        }, 
+        990: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 2,
+            enabled: true
+        },
+        1: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            enabled:false
+        },
+    }
+});
+
+$('.js-slick').slick({
+    arrows:true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    fade: true,
+    responsive: [
+        {
+            breakpoint: 990,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
+});
